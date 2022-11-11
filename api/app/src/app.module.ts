b8883ee27fee189f2f0deade00ece5c1348731ee
lewis941001 <lewis1004@naver.com>
2022-11-12 01:06:13 +0900

    [Add]Connect DB

--- api/app/src/app.module.ts
+++ api/app/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.DB_HOST_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      // entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      // retryDelay: 3000,
+      // retryAttempts: 10
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
