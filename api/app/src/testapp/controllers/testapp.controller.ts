17740344595c481bf9a9bbaab9697557ff86d230
lewis941001 <lewis1004@naver.com>
2022-11-12 12:54:16 +0900

    [Fix]controller with service

--- api/app/src/testapp/controllers/testapp.controller.ts
+++ api/app/src/testapp/controllers/testapp.controller.ts
@@ -1,30 +1,40 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TestappService } from '../services/testapp.service';
 
 @Controller('api/testapp')
 export class TestappController {
     
+    constructor (
+        private testappService : TestappService // service 불러오기
+    ) {}
+
     @Get()
     getAll() {
-        return [1, 2, 3];
+        // return [1, 2, 3];
+        return this.testappService.findAll();
     }
 
     @Get(':id')
     getOne(@Param('id') id: number){
-        return id;
+        // return id;
+        return this.testappService.findOne(id);
     }
 
     @Post()
     create(@Body() body: any) {
-        return body;
+        // return body;
+        return this.testappService.create(body);
     }
 
     @Put(':id')
     update(@Param('id') id:number, @Body() body: any) {
-        return body;
+        // return body;
+        return this.testappService.update(id, body);
     }
 
     @Delete(':id')
     delete(@Param('id') id : number) {
-        return true;
+        // return true;
+        return this.testappService.delete(id);
     }
 }
