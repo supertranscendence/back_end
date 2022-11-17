45b8c1baf541261214c72a96bcd23ed34caca7ec
lewis941001 <lewis1004@naver.com>
2022-11-17 19:58:47 +0900

    [Fix] import mainModules

--- api/app/src/app.module.ts
+++ api/app/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TestappModule } from './testapp/testapp.module';
 import { typeORMConfig } from './typeorm.config';
+import { AchievementsModule } from './achievements/achievements.module';
+import { FriendsModule } from './friends/friends.module';
+import { UsersModule } from './users/users.module';
+import { AchievementsCodeModule } from './achievements_code/achievements_code.module';
+// import { GameRepository } from './game/repository/game.repository';
+import { GameModule } from './game/game.module';
+// import { TestModule } from './test/test.module';
+
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    TestappModule,
+    // TestappModule,
+    GameModule,
+    AchievementsModule,
+    FriendsModule,
+    UsersModule,
+    AchievementsCodeModule,
   ],
   controllers: [],
   providers: [],
