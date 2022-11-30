729ad449de11ab6707748e6d9e047a8a02fd4c4b
gilee <gilee@student.42seoul.kr>
2022-11-30 20:41:37 +0900

    [FIX #108]

--- api/app/src/auth/auth.controller.ts
+++ api/app/src/auth/auth.controller.ts
@@ -9,24 +9,28 @@ import {ConfigService} from "@nestjs/config";
 
 @Controller('api/auth')
 export class AuthController {
-
+    readonly frontend_url;
+    readonly domain;
     constructor (
         private auth : AuthService,
         private config: ConfigService
-    ) {}
+    ) {
+        this.frontend_url = this.config.get('FRONTEND_URL');
+        this.domain = this.config.get('DOMAIN');
+    }
     @Get('/ft/redirect')
     @UseGuards(AuthGuard('42'))
     @HttpCode(302)
-    @Header('Access-Control-Allow-Origin', '*')
+    @Header('Access-Control-Allow-Origin', 'https://gilee.click')
     @Header('Access-Control-Allow-Credentials', 'true')
     async ftLoginCallback(@Req() req, @Res() res){
         console.log(req.user);
         let date: Date = new Date();
         date.setDate(date.getTime() + 1000 * 10);
-        res.cookie('accessToken', req.user.ac, {domain: this.config.get('DOMAIN'), expire: date.toUTCString(), sameSite: 'Strict'});
-        res.cookie('refreshToken', req.user.re, {domain: this.config.get('DOMAIN'), expire: date.toUTCString(), sameSite: 'Strict'});
+        res.cookie('accessToken', req.user.ac, {domain: this.domain, expire: date.toUTCString(), sameSite: 'Strict'});
+        res.cookie('refreshToken', req.user.re, {domain: this.domain, expire: date.toUTCString(), sameSite: 'Strict'});
         //res.redirect(this.config.get(FRONTEND_URL + '/login');
-        console.log(this.config.get('FRONTEND_URL'));
-        res.redirect(this.config.get('FRONTEND_URL'));
+        console.log(this.frontend_url);
+        res.redirect(this.frontend_url);
     }
 }
