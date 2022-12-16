e71707d7056be0efda791669fcb0c50d1b3bb0a8
lewis941001 <lewis1004@naver.com>
2022-12-16 16:59:18 +0900

    [Fix]

--- api/app/src/users/users.module.ts
+++ api/app/src/users/users.module.ts
@@ -4,10 +4,11 @@ import { UsersRepository } from './repository/users.repository';
 import { UsersService } from './services/users.service';
 import { AuthService } from '../auth/auth.service';
 import { AuthRepository } from '../auth/auth.repository';
+import { FriendsRepository } from '../friends/repository/friends.repository';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, AuthService, AuthRepository],
+  providers: [UsersService, UsersRepository, AuthService, AuthRepository, FriendsRepository],
 })
 export class UsersModule {}
