08ee1b93b0059c99c9728de9e4980db402cad4d1
lewis941001 <lewis1004@naver.com>
2022-12-27 02:42:26 +0900

    [Add] Queue Size

--- api/app/src/gateway/Queue.ts
+++ api/app/src/gateway/Queue.ts
@@ -4,15 +4,30 @@ export class Queue  {
   storage: IUser[];
   head : number;
   tail : number;
+  size : number;
   constructor() {
     this.storage = [];
     this.head = 0;
     this.tail = 0;
+    this.size = 0;
+  }
+
+  data() {
+    console.log('----------- data ------------')
+    console.log(this.storage)
+    console.log(this.head)
+    console.log(this.tail) // 안이 증가하면 tail이 뒤로
+    console.log(this.size) // 안의 내용물의 갯수
   }
 
   enqueue(element : IUser) {
     this.storage[this.tail] = element;
     this.tail++;
+    this.size++;
+  }
+
+  getSize() :number {
+    return (this.size);
   }
 
   getHead() :number{
@@ -35,6 +50,7 @@ export class Queue  {
     }
     this.storage[this.head] = null;
     this.head++;
+    this.size--;
     return removed;
   }
 
@@ -42,19 +58,42 @@ export class Queue  {
     for (const ele of this.storage) {
       if (ele)
         if (ele.intra == element.intra)
+        {
+          console.log('equal: ' , ele.intra);
           return true;
+        }
     }
       return false;
   }
 
+  //지우기가 안됨
   delete(clientId : string) : boolean {
-    for (let i of this.storage) {
-      if (i) {
-        if (i.client.id == clientId)
-          i = null;
-      return true;
+    
+    for(var i = 0; i < this.tail; i++){ 
+      if (this.storage[i] != null) 
+        if (this.storage[i].client.id === clientId) { 
+        this.storage[i] = null;
+        console.log('true');
+        this.size--;
+        return true;
       }
     }
+    console.log('false');
     return false;
+
+    // for (let i of this.storage) {
+    //   if (i) {
+    //     console.log(i)
+    //     if (i.client.id == clientId)
+    //     {  
+    //       i = null; // 여기서 지우는 방식을 달리하면 될거 같음!
+    //       console.log(i)
+    //     }
+    //     console.log('ture');
+    //     return true;
+    //   }
+    // }
+    // console.log('false');
+    // return false;
   }
 }
